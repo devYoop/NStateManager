729e1fd2566f75d7ddf7acb6e149c6c83f2929a1
devYoop <50983256+devYoop@users.noreply.github.com>
2019-12-26 12:06:25 +0100

    Update AssemblyInfo.cs

--- Source/NStateManager/AssemblyInfo.cs
+++ Source/NStateManager/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.2.5")]
+[assembly: AssemblyFileVersion("1.2.5")]
